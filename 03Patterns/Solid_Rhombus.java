4e988a1d98485319c1ab8bb12bf4c8190412e7da
RitikRK96 <ritikkumar9525586511@gmail.com>
2025-12-06 19:22:27 +0530

    Solid Rhombus pattern

--- 03Patterns/Solid_Rhombus.java
+++ 03Patterns/Solid_Rhombus.java
@@ -1,12 +1,12 @@
 public class Solid_Rhombus {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 1; i < 2 * n; i++) {
-            for (int j = 1; j < n; j++) {
-                // for spaces
-                if (i == n) {
-                    System.out.print("*");
-                }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for(int j = 1; j <= n; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
